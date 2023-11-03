e9097f903dd1765effd2cbab292859ee881c0a10
Rushaan78 <rs2095@hw.ac.uk>
2023-11-03 09:54:18 +0400

    updated code

--- WEEK 4/main.js
+++ WEEK 4/main.js
@@ -12,7 +12,7 @@ var myCity = ["Dubai", "UAE", "Dubai Mall"];
 
 console.log(myCity[0]); // Outputs: Dubai
 
-var myCities = [
+var myCities = [ 
     {
     "name": "Dubai", 
     "country": "UAE", 
@@ -27,22 +27,42 @@ var myCities = [
 
 console.log(myCities[1].country); // Outputs: Italy
 
+
+
 var cityContainer = document.getElementById("city-info");
 var btn = document.getElementById("btn");
 btn.addEventListener("click", function(){
 var ourRequest = new XMLHttpRequest();
-ourRequest.open('GET', 'https://rs2095.github.io/rs2095.github.io/cities1.json');
+ourRequest.open('GET', 'https://your-github-username.github.io/your-repositoryname/cities1.json');
 ourRequest.onload = function() {
 var ourData = JSON.parse(ourRequest.responseText);
 renderHTML(ourData);
+btn.classList.add("hide-me");
 };
 ourRequest.send();
 });
 function renderHTML(data){
 var htmlString = "";
 for (i=0; i<data.length; i++){
-htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ".</p>"
-;
+htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ",</br> Where you can enjoy indoor places like: " ;
+for (ii = 0; ii < data[i].places.indoor.length; ii++) {
+// Loop through the indoor places of the current city.
+if (ii == 0) {
+htmlString += data[i].places.indoor[ii];
+} else {
+htmlString += ", and " + data[i].places.indoor[ii];
+}
+}
+htmlString += '. & enjoy outdoor places like: ';
+// Loop through the outdoor places of the current city.
+for (ii = 0; ii < data[i].places.outdoor.length; ii++) {
+if (ii == 0) {
+htmlString += data[i].places.outdoor[ii];
+} else {
+htmlString += " and " + data[i].places.outdoor[ii];
+}
+}
+htmlString += '.</p>';
 }
 cityContainer.insertAdjacentHTML('beforeend' , htmlString);
 }
