0d70d1903081c44da571aaab9fb01bd48ad20919
Rushaan78 <rs2095@hw.ac.uk>
2023-11-03 09:43:14 +0400

    added week 4 folder and added code

---
+++ WEEK 4/main.js
@@ -0,0 +1,48 @@
+var myCity = {
+    "name": "Dubai",
+    "country": "UAE",
+    "place": "Dubai Mall"
+    };
+
+
+console.log(myCity.name); // Outputs: Dubai
+console.log(myCity.country); // Outputs: UAE
+
+var myCity = ["Dubai", "UAE", "Dubai Mall"];
+
+console.log(myCity[0]); // Outputs: Dubai
+
+var myCities = [
+    {
+    "name": "Dubai", 
+    "country": "UAE", 
+    "place": "Dubai Mall"
+    },
+    {
+    "name": "Milan", 
+    "country": "Italy", 
+    "place": "Duomo Di Milano"
+    }
+    ];
+
+console.log(myCities[1].country); // Outputs: Italy
+
+var cityContainer = document.getElementById("city-info");
+var btn = document.getElementById("btn");
+btn.addEventListener("click", function(){
+var ourRequest = new XMLHttpRequest();
+ourRequest.open('GET', 'https://rs2095.github.io/rs2095.github.io/cities1.json');
+ourRequest.onload = function() {
+var ourData = JSON.parse(ourRequest.responseText);
+renderHTML(ourData);
+};
+ourRequest.send();
+});
+function renderHTML(data){
+var htmlString = "";
+for (i=0; i<data.length; i++){
+htmlString += "<p>" + data[i].name + " is a city in " + data[i].country + ".</p>"
+;
+}
+cityContainer.insertAdjacentHTML('beforeend' , htmlString);
+}
