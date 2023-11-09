cf286d3559a6c0a487003a84d29440c5d26178e9
Rushaan78 <rs2095@hw.ac.uk>
2023-11-09 19:29:41 +0400

    added CSS file and started JavaScript

---
+++ WEEK 4/WEATHER/app.js
@@ -0,0 +1,5 @@
+const API = "65d02db73c72649b1abf934a2a765b89";   //api to get weather information
+
+const cityInput = document.getElementById("cityInput"); //constant variable declarations
+const whenClicked = document.getElementById("btn");
+const weatherInformation = document.getElementById("weather-info");
