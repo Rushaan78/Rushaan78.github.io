e2fc8c4e9951ec8d4c81bea773c15d5373f229a5
Rushaan78 <rs2095@hw.ac.uk>
2023-11-17 22:12:27 +0400

    completed task 1 and 2

---
+++ WEEK 5/mysql.session.sql
@@ -0,0 +1,10 @@
+  <!-- Database was created in SQL-Workbench then connection was established, which is why the 'CREATE DATABASE user_authentication' query is not seen here  -->
+
+CREATE TABLE user_data (
+
+user_id INT AUTO_INCREMENT,
+user_name VARCHAR(100),
+email VARCHAR(100), 
+password VARCHAR(255)
+
+);
